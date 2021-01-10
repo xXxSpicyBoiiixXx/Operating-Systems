dad49d328bb199d539498826a990d7b8dc12e33b
Md Ali <66300613+xXxSpicyBoiiixXx@users.noreply.github.com>
2021-01-10 01:45:38 -0600

    Problems with X Compiling

--- Development-OS-Wiki/boot.s
+++ Development-OS-Wiki/boot.s
@@ -1,3 +1,8 @@
+/*
+To compile type the below command in the terminal
+gcc -c boot.s -o boot.o
+*/
+
 .set ALIGN,	1<<0 /* align loaded modules on page boundaries */
 .set MEMINFO,	1<<1 /* provides memory mapping */ 
 .set FLAGS, 	ALIGN | MEMINFO /* Multiboot flag field */ 
@@ -51,5 +56,15 @@ _start:
 	/* 
 	If the system has nothing more to do, put the computer into an infinite loop. To do that: 
 	1) Disable interrupts with cli (clear interrupt enable in eflags). They are already disabled by the bootloader, so this is not needed. Mind that you might later enable interrupts and return from kernel_main (which is sort of nonsensical to do).
-	2) 
+	2) Wait for the next interrupt to arrive with hlt (halt instruction) Since they are disabled, this will lock up the computer.
+	3) Jump to the hlt instruction if it ever wakes up due to a non-maskable interrupt occuring or due to system management mode. 
 	*/
+	cli 
+1:	hlt 
+	jmp lb
+
+/*
+Set the size of the _start symbol to the current location '.' minus its start. This is useful when debugging or when you implement call tracing. 
+*/ 
+.size _start, . -_start 
+
